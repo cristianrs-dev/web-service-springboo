e966ec844e4f7ed72fbce46355542b9d5218f55d
27SARRAZIN <sarrazinr5@gmail.com>
2025-02-07 15:10:19 -0400

    User entitie Resource

---
+++ src/main/java/com/academy/curso/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.academy.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.academy.curso.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1,"maria","maria@gmail.com","9999-999","123");
+		return ResponseEntity.ok().body(u);
+	}
+}
