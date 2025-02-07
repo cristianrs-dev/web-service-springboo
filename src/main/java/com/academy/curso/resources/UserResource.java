29ff0211f6fc2912a889c078c212d73f5814599a
27SARRAZIN <sarrazinr5@gmail.com>
2025-02-07 16:17:38 -0400

    JPA repository, dependency injection, database seeding

--- src/main/java/com/academy/curso/resources/UserResource.java
+++ src/main/java/com/academy/curso/resources/UserResource.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.academy.curso.entities.User;
 
+
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	@GetMapping
 	public ResponseEntity<User> findAll(){
+		
 		User u = new User(1,"maria","maria@gmail.com","9999-999","123");
 		return ResponseEntity.ok().body(u);
 	}
